7162022c356ef4468a3eb96f71b1883785cb3c3a
marcustung <marcustung116@gmail.com>
2017-08-06 00:15:48 +0800

    First version
    First version

---
+++ String-average/String-average/StringAverage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_average
+{
+	public class Kata
+	{
+		public string StringAverage(string str)
+		{
+			string result = string.Empty;
+			int sum = 0;
+			string[] numbers = new string[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+			// verify
+			if (string.IsNullOrEmpty(str))
+				return "n/a";
+
+			// replace
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				str = str.Replace(numbers[i].ToString(), i.ToString());
+			}
+
+			// split 
+			string[] array = str.Split(' ');
+			
+			foreach (string item in array)
+			{
+				int itemNumber = 0;
+
+				if (!int.TryParse(item, out itemNumber))
+					return "n/a";
+
+				sum += itemNumber;
+			}
+			
+			result = numbers[int.Parse((sum / array.Length).ToString())];
+
+			return result;
+		}
+	}
+}
---
+++ String-average/String-averageTests1/KataTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using String_average;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_average.Tests
+{
+	[TestFixture()]
+	public class KataTests
+	{
+		[Test()]
+		[TestCase("zero nine five two", "four")]
+		[TestCase("four six two three", "three")]
+		[TestCase("one two three four five", "three")]
+		[TestCase("five four", "four")]
+		[TestCase("zero zero zero zero zero", "zero")]
+		[TestCase("one one eight one", "two")]
+		[TestCase("", "n/a")]
+		public void StringAverageTest(string str, string result)
+		{
+			// Arrange
+			Kata kata = new Kata();
+
+			// Act 
+			string stringAverage = kata.StringAverage(str);
+
+			// Assert
+			Assert.AreEqual(stringAverage, result);
+		}
+	}
+}
